56546e9d125ced86fafeb47baf9c0a3049bb1190
Guilherme Lessa <gui.silva.lessa@gmail.com>
2026-04-15 13:01:09 -0300

    Update echo.js

--- frontend/React/src/echo.js
+++ frontend/React/src/echo.js
@@ -1,16 +1,16 @@
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 
 window.Pusher = Pusher;
 
 const echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT,
-    wssPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: false,
-    enabledTransports: ['ws'],
+  broadcaster: "reverb",
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
+  wsPort: 80,
+  wssPort: 443,
+  forceTLS: true,
+  enabledTransports: ["ws", "wss"],
 });
 
 export default echo;
